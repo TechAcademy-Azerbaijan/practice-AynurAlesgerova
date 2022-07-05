089dfa5bae8cc42a46ad0ec29c4a076faca32c89
AynurAlesgerova <105042078+AynurAlesgerova@users.noreply.github.com>
2022-07-05 11:27:40 +0400

    Update main.js

--- task-6/main.js
+++ task-6/main.js
@@ -1,9 +1,10 @@
 const prompt = require('prompt');
 prompt.start();
 
+prompt.get('input', function(err,result) {
+   
+    let a= parseInt(result.input.split(',')[0])
+    let b= parseInt(result.input.split(',')[1])
+    console.log(a*b);
 
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+  });   
