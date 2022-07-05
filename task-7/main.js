8ff548b0224cc25aea41af8bd478a9fe8b0171f9
AynurAlesgerova <105042078+AynurAlesgerova@users.noreply.github.com>
2022-07-05 11:40:35 +0400

    Update main.js

--- task-7/main.js
+++ task-7/main.js
@@ -1,9 +1,17 @@
 const prompt = require('prompt');
 prompt.start();
 
+prompt.get('input', function(err,result) {
+   
+    let a= parseInt(result.input)
+  
+    for (let i = 1; i < a; i++) {
 
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+        if (i*i<a) {
+            console.log(i*i);
+        }
+        
+        
+    }
+
+  });   
