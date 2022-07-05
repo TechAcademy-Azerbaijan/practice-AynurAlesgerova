75f2f3c4b18426b69ea5eaf591dba1125f6662c7
AynurAlesgerova <105042078+AynurAlesgerova@users.noreply.github.com>
2022-07-05 11:18:47 +0400

    Update main.js

--- task-5/main.js
+++ task-5/main.js
@@ -1,9 +1,18 @@
 const prompt = require('prompt');
 prompt.start();
 
+prompt.get('input', function(err,result) {
+    let array=result.input.split(',')
 
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+    let max=0
+    for (let i = 0; i < array.length; i++) {
+        if (max<array[i]) {
+            max=array[i]
+        }
+        
+    }
+    console.log(max);
+
+   
+    
+  });   
