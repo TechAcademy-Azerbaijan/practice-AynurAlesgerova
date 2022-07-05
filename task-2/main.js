0a73cc273be11282d33d377d24f9b553063d154e
AynurAlesgerova <105042078+AynurAlesgerova@users.noreply.github.com>
2022-07-05 10:56:22 +0400

    Update main.js

--- task-2/main.js
+++ task-2/main.js
@@ -1,9 +1,22 @@
 const prompt = require('prompt');
 prompt.start();
 
+prompt.get('input', function(err,result) {
+    let a=parseInt(result.input)
 
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+    let count=0
+    for (let i = 1; i <= a; i++) {
+        if(a%i==0){
+            count=count+1
+        }
+        
+    }
+    
+    if (count<=2) {
+        console.log("Yes");
+    }
+    else{
+        console.log("No");
+    }
+    
+  });   
